c5787b0bcbb094b7c080d2ab6fef74d133a1e003
Sivaram Dheeraj Vishnubhotla <dheerajvishnubhotla@gmail.com>
2026-08-14 22:18:49 -0400

    Disable local development toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://sivaram-dheeraj-vishnubhotla.github.io',
   base: '/',
-  output: 'static'
+  output: 'static',
+  devToolbar: {
+    enabled: false
+  }
 });
